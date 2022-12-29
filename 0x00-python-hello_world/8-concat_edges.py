6c6672d42cd8a348e12ecfa9aab82c8fb96f7fb7
Susu-spec <suleimansuwaibat@gmail.com>
2022-12-29 08:29:52 -0800

    Modify: string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriente\
 d programming language that combines remarkable power with very clear syntax"
-str = "{} {} {}".format(str[39:66], str[-22:-18], str[:6])
-print(str)
+print("{} {} {}".format(str[39:66], str[-22:-18], str[:6]))
