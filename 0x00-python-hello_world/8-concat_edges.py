ca395804bc147624355152ce30b2cabd10064686
Susu-spec <suleimansuwaibat@gmail.com>
2022-12-29 08:35:50 -0800

    Modify: string literals fix?

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriente\
 d programming language that combines remarkable power with very clear syntax"
-print("{} {} {}".format(str[39:66], str[-22:-18], str[:6]))
+str = "{} {} {}".format(str[39:66], str[-22:-18], str[:6])
+print(str)
