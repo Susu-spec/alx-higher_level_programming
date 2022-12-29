a2e2db6220c26ef767771f72c4338427a070c6b9
Susu-spec <suleimansuwaibat@gmail.com>
2022-12-29 03:31:11 -0800

    Add: 8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, objected-oriented programming language that combines remarkable power with very clear syntax"
+print(f"{str[39:68]} {str[-22:-18]} {str[:6]}")
