960885cb03831a16023824a7870e93b308082dd5
Susu-spec <suleimansuwaibat@gmail.com>
2022-12-29 07:49:24 -0800

    Modify: no string literals now

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, objected-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:68] + " " + str[-22:-18] + " " + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "{} {} {}".format(str[39:67], str[-22:-18], str[:6])
+print(str)
+
