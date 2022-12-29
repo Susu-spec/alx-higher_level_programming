ccd43ec77f750479479dd4dfca1401bee13628f0
Susu-spec <suleimansuwaibat@gmail.com>
2022-12-29 08:26:22 -0800

    Modify: char number in line

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriente\
-d programming language that combines remarkable powe\
-r with very clear syntax"
+d programming language that combines remarkable power with very clear syntax"
 str = "{} {} {}".format(str[39:66], str[-22:-18], str[:6])
 print(str)
