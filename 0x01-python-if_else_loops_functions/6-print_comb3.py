c5cd880ac47410fda41fbd1e9b77d9d72976bb8a
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-01 03:27:53 -0800

    Modify: break added

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,5 @@ for i in range(10):
             continue
         if (i == 8 and j == 9):
             print('{}{}'.format(i, j), end='\n')
+            continue
         print('{}{}'.format(i, j), end=', ')
