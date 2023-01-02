eb7f2cbf61de9d7f5117001389bb93e433141890
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-01 21:48:05 -0800

    Add: 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# check if a character is a letter
+def is_letter(ch):
+    ch_code = ord(ch)
+    if (65 <= ch_code <= 90) or (97 <= ch_code <= 122):
+        return True
+    else:
+        return False
+
+# check if uppercase
+def is_upper(ch):
+    if (65 <= ord(ch) <= 90):
+        return True
+    else:
+        return False
+
+# convert to uppercase
+def uppercase(str):
+    result = ''
+    for letter in str:
+        if (is_letter(letter) == False) or (is_upper(letter) == True):
+            result += letter
+            continue
+        ch_code = ord(letter) - 32
+        char = chr(ch_code)
+        result += char
+    print(result)
