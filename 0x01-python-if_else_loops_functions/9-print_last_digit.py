24a7750a508a8fa8df6dbfa7a81c85ac1057de02
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-02 01:03:39 -0800

    Modify: invalid argument error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if (not (isinstance(number, int))):
-        return (0)
     number = abs(number)
     last_digit = number % 10
     print('{}'.format(last_digit), end='')
