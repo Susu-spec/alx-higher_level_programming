19c8c971e65e0dd2111a37f30ea7a18852aa0d86
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-04 06:45:44 -0800

    Add: 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    length = len(str)
+    copy = []
+    for i in range(length):
+        if i != n:
+            copy.append(str[i])
+    copy = "".join(copy) # Convert list to string
+    return (copy)
+
