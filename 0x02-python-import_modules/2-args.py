27a9f06bc8da770760d250d402716bc9a1025c9f
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-06 04:21:06 -0800

    Add: 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+args = len(argv[1:])
+if (args == 0):
+    print("0 arguments.")
+else:
+    print("{} argument{}:".format(args, 's' if args > 1 else ''))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
