8438ef76109d7798ef8dbda8bc56c4d0e5732437
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-06 02:27:52 -0800

    Add: 0-add.py

--- 0x02-python_import_modules/0-add.py
+++ 0x02-python_import_modules/0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-
-
-def main():
-    a = 1
-    b = 2
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-
-
 #  run directly not when called via import
 if __name__ == "__main__":
-    main()
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
