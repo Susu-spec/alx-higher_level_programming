b817156bab88b6e14eff1e313c035bba3bedabc7
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-06 04:29:27 -0800

    Modify: run only when executed directly

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
-args = len(argv[1:])
-if (args == 0):
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(args, 's' if args > 1 else ''))
+def main():
+    args = len(argv[1:])
+    if (args == 0):
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(args, 's' if args > 1 else ''))
     for i, arg in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, arg))
+
+
+if __name__ == "__main__":
+    main()
