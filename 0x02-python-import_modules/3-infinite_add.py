0dc3129538c84d6999a565ae756374a7fe742abd
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-07 06:35:44 -0800

    Add: 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    result = 0
+    for arg in argv[1:]:
+        result += int(arg)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
