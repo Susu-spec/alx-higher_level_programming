e4a729338d1ff31a6cb06f505ba61c886196266d
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-16 03:05:08 -0800

    Modify: allow traceback error with char input

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = len(my_list) - 1
     # if list is empty, return none
     if not my_list:
         return None
+    idx = len(my_list) - 1
+    # similar to reverse()
     for i in range(idx, -1, -1):
-        if type(my_list[i]) == int:
-            print("{:d}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
