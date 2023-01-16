b94cbb72452d53b62b049121dfbc9588ce4f729d
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-16 05:54:51 -0800

    Modify: handle empty list case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for lists in matrix:
         for elem in lists:
             if elem == lists[len(lists) - 1]:
