4d40bb330673b51f5b02c9e3f4e6709c61633d75
Susu-spec <suleimansuwaibat@gmail.com>
2023-01-17 04:34:58 -0800

    Modify: handle negative int list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    maxi = 0
-    for elem in my_list:
-        if elem > maxi:
-            maxi = elem
-    return maxi
+    my_list.sort()
+    return my_list[-1]
